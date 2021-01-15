18af31337154298320f366cb800fe127104856ab
mikolajmarcinczak <mikolajmarcinczak@users.noreply.github.com>
2021-01-15 02:53:46 +0100

    Dodaj pliki projektów.

---
+++ Projekt ASP/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Models.Movie movie = new Models.Movie()
+            {
+                Title = "The Godfather",
+                ReleaseDate = new DateTime(1972, 3, 24)
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ Projekt ASP/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace Projekt_ASP
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession();
+            services.AddMvc(option => 
+            option.EnableEndpointRouting = false
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStaticFiles();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseRouting();
+
+            app.UseSession();
+            app.UseMvc(routes => {
+                routes.MapRoute("Names", "Sub/{action=Names}/{id?}", new { controller = "Sub" });
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
